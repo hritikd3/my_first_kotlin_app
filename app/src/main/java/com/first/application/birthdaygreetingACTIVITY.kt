249c2a3e918b45f24c67edc0d5af71b78e175d60
hritikd3 <hritikdangi3@gmail.com>
2022-12-18 14:10:15 +0530

    added files for birthday greet app

---
+++ app/src/main/java/com/first/application/birthdaygreetingACTIVITY.kt
@@ -0,0 +1,11 @@
+package com.first.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class birthdaygreetingACTIVITY : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthdaygreeting)
+    }
+}
